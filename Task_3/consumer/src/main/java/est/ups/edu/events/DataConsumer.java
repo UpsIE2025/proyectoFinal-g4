e746b375060776bd51f851534510adcefe0acd34
Edisson <edissonredrovan@gmail.com>
2025-02-23 15:48:07 -0500

    Se cambia en consumer  para obtener semeste y carrera, en kafka llave y valor

--- Task_3/consumer/src/main/java/est/ups/edu/events/DataConsumer.java
+++ Task_3/consumer/src/main/java/est/ups/edu/events/DataConsumer.java
@@ -3,16 +3,17 @@ package est.ups.edu.events;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import io.smallrye.reactive.messaging.kafka.Record;
 
 @ApplicationScoped
 @Slf4j
 public class DataConsumer {
 
     @Incoming("data-input")
-    public void receive(String message) {
+    public void receive(Record<Integer, String> record){
 
-        log.info(message);
-
+        log.info("Semestre: "+ record.key()+" ,Carrera: "+ record.value());
+        
     }
 
 }
