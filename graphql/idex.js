5a280be009041d8c4e1cd12228e13bc6806ae3d7
RomelAvila2001 <ravilaf1@est.ups.edu.ec>
2025-02-27 00:19:54 -0500

    modificaccion index

--- graphql/idex.js
+++ graphql/idex.js
@@ -5,3 +5,23 @@ const typeDefs = require('./graphql/typeDefs'); // Importa el esquema GraphQL
 const resolvers = require('./graphql/resolvers'); // Importa los resolvers
 
 
+const app = express();
+
+app.use(cors());
+
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Servidor GraphQL corriendo en http://localhost:${PORT}/graphql`);
+  });
+}
+
+startServer();
