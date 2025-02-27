ea7ad70dd4c4a48267009d6864e3a0c696297fdb
RomelAvila2001 <ravilaf1@est.ups.edu.ec>
2025-02-27 00:16:50 -0500

    resolvers

--- graphql/idex.js
+++ graphql/idex.js
@@ -2,4 +2,6 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const cors = require('cors');
 const typeDefs = require('./graphql/typeDefs'); // Importa el esquema GraphQL
+const resolvers = require('./graphql/resolvers'); // Importa los resolvers
+
 
